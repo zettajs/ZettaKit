8a38d121e8d755a60a192917cb91c7cfbb03d444
Matthew Dobson <mdobson4@gmail.com>
2016-01-21 11:15:13 -0500

    Fixing bad include statement.

--- Pod/Classes/ZIKPubSubBroker.h
+++ Pod/Classes/ZIKPubSubBroker.h
@@ -24,7 +24,7 @@
 //  THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
-#import "include/ispdy.h"
+#import "ispdy.h"
 
 /**
  The `ZIKPubSubBroker` object represents a the reciever for SPDY push streams.
