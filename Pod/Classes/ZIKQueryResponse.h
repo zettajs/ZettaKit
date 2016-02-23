f981fa785d97ae7d3728685028789e5f4fb8e7b5
Matthew Dobson <mdobson4@gmail.com>
2016-02-23 15:22:19 -0500

    Adding documentation. Adding a new iOS requirement.

--- Pod/Classes/ZIKQueryResponse.h
+++ Pod/Classes/ZIKQueryResponse.h
@@ -12,14 +12,57 @@
 
 @interface ZIKQueryResponse : NSObject
 
+/**
+ Links included with the query response.
+ */
 @property (nonatomic, retain, readonly) NSArray *links;
+
+/**
+ The devices that fulfill the query.
+ */
 @property (nonatomic, retain) NSArray *devices;
+
+/**
+ The server the query was executed against.
+ */
 @property (nonatomic, retain, readonly) NSString *server;
+
+/**
+ The CAQL query language used to filter devices.
+ */
 @property (nonatomic, retain,readonly) NSString *ql;
 
+///---------------------------
+/// @name Initialization
+///---------------------------
+
+/**
+ Initializes a `ZIKQueryResponse` with the specified websocket document in `NSDictionary` form.
+ 
+ @param data The base siren document to create the stream entry with.
+ 
+ @return The newly-initialized `ZIKQueryResponse` object.
+ */
+
 -(instancetype) initWithDictionary:(NSDictionary *) data;
+
+/**
+ Initializes a `ZIKQueryResponse` with the specified websocket document in `NSDictionary` form.
+ 
+ This is the designated initializer.
+ 
+ @param data The base siren document to create the stream entry with.
+ 
+ @return The newly-initialized `ZIKQueryResponse` object.
+ */
+
 +(instancetype) initWithDictionary:(NSDictionary *) data;
 
+/**
+ Creates a `ZIKStream` of results.
+ 
+ @return `ZIKStream` that will update with devices that fulfill the query.
+ */
 -(ZIKStream *) resultsStream;
 
 @end
